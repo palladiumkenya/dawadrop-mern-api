f6592ffbb08153438af211597ab911b55a0462c9
Laurent Ouma <lawiomosh3@gmail.com>
2023-07-07 13:44:06 +0300

    Setup the environment and made cofigurations

---
+++ index.js
@@ -0,0 +1,25 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+dotenv.config();
+const config = require("config");
+console.log(`[-]App name: ${config.get("name")}`);
+console.log(`[-]Database: ${config.get("db")}`);
+mongoose
+  .connect(config.get("db"))
+  .then((result) => {
+    console.log("[-]Connected to database Successfully");
+  })
+  .catch((err) => console.log("[x]Could not connect to database" + err));
+const app = express();
+
+app.use(express.json());
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  console.log("[-]Morgan Enabled");
+}
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server running on port " + port + " ....");
+});
