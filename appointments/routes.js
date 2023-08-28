1e2561486119d99e3fda9d6dca6c93db3e7b8bda
Laurent Ouma <lawiomosh3@gmail.com>
2023-08-28 14:44:35 +0300

    Care receiver upcoming appointment endpoints added

--- appointments/routes.js
+++ appointments/routes.js
@@ -1,13 +1,17 @@
 const { Router } = require("express");
 const { getPatientAppointments } = require("./api");
+const auth = require("../middleware/auth");
+const {
+  getCareReceiverUpcomingAppointments,
+  getAllMyCareReceiversUpcomingAppointments,
+} = require("./views/careReceivers");
 
 const router = Router();
 
-router.get("/", async (req, resp) => {
-  const q = req.query;
-  const cccNumber = q.patient;
-  const appointments = await getPatientAppointments(cccNumber);
-  console.log("Heare");
-  resp.json({ results: appointments ? appointments : [] });
-});
+router.get(
+  "/care-receiver/:cccNumber",
+  auth,
+  getCareReceiverUpcomingAppointments
+);
+router.get("/care-receiver", auth, getAllMyCareReceiversUpcomingAppointments);
 module.exports = router;
